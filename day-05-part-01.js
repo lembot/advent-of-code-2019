112647225e17e958b2c9d844ff421c6d6be4be30
lembot <l3mb0t@gmail.com>
2019-12-06 16:40:48 -0500

    Update day-05-part-01.js

--- day-05-part-01.js
+++ day-05-part-01.js
@@ -1,48 +1,67 @@
+/**
+ * Takes initial memory and performs actions based on the provided
+ * instructions.
+ * @param {string} input Comma delimited list representing initial memory
+ *                       for the program.
+ */
 const intcodeProgram = input => {
   let memory = input.split(',').map(num => {
     return parseInt(num)
   })
   
-    for (let i = 0; memory[i] !== 99;) {
-      let instruction = memory[i].toString()
-      let opCodeLen = instruction.length
-      let opCode = parseInt(instruction.slice(instruction.length - 2, instruction.length))
+  for (let i = 0; memory[i] !== 99;) {
+    let instruction = memory[i].toString()
+    let opCodeLen = instruction.length
+    // The opcode CAN be given as 01, in which case it needs to be trimmed to just 1
+    let opCode = parseInt(instruction.slice(instruction.length - 2, instruction.length))
 
-      let modes = instruction.split('').reverse().join('').slice(2, instruction.length)
-      let p1Mode = modes[0] ? parseInt(modes[0]) : 0
-      let p2Mode = modes[1] ? parseInt(modes[1]) : 0
-      let p3Mode = modes[2] ? parseInt(modes[2]) : 0
-      let val1 = ''
-      let val2 = ''
+    // There are also potential modes that are important, but not always provided...
+    let modes = instruction.split('').reverse().join('').slice(2, instruction.length)
+    let p1Mode = modes[0] ? parseInt(modes[0]) : 0
+    let p2Mode = modes[1] ? parseInt(modes[1]) : 0
+    let p3Mode = modes[2] ? parseInt(modes[2]) : 0
+    let val1 = ''
+    let val2 = ''
 
     switch (opCode) {
+
+      // Addition
       case 1:
         val1 = p1Mode ? memory[i + 1] : memory[memory[i + 1]]
         val2 = p2Mode ? memory[i + 2] : memory[memory[i + 2]]
         memory[memory[i + 3]] = val1 + val2
         i = i + 4
         break
+
+      // Multiplication
       case 2:
         val1 = p1Mode ? memory[i + 1] : memory[memory[i + 1]]
         val2 = p2Mode ? memory[i + 2] : memory[memory[i + 2]]
         memory[memory[i + 3]] = val1 * val2
         i = i + 4
         break
+
+      // Store value
       case 3:
         memory[memory[i + 1]] = parseInt(prompt())
         i = i + 2
         break
+
+      // Retrieve value
       case 4:
         console.log(p1Mode ? memory[i + 1] : memory[memory[i + 1]])
         i = i + 2
         break
+
+      // Finish
       case 99:
         return(1)
         break
+
+      // Problem :(
       default:
         console.error('IDK')
         return(0)
-        break
     }
   }
   return memory
