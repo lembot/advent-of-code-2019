0ab64bbfd5304ed8fc584fd089d333023b4451ad
lembot <l3mb0t@gmail.com>
2019-12-06 16:26:32 -0500

    Update day-04-part-01.js

--- day-04-part-01.js
+++ day-04-part-01.js
@@ -1,4 +1,6 @@
-// Despite the name, it expects a number converted to a string.
+// MUST BE A NUMBER CONVERTED TO A STRING
+// Determines if this number has adjacent repeated digits.
+// Example: 112233 (GOOD!) or 123456 (BAD!)
 const hasRepeatedAdjacent = num => {
   for (let i = 0; i < num.length - 1; i++) {
     if (num[i] === num[i + 1]) {
@@ -8,7 +10,9 @@ const hasRepeatedAdjacent = num => {
   return false
 }
 
-// Despite the name, it expects a number converted to a string.
+// MUST BE A NUMBER CONVERTED TO A STRING
+// Determines if the digits (left to right) descend.
+// Example: 123456 (GOOD!) or 222221 (BAD!)
 const doesNotDescend = num => {
   for (let i = 0; i < num.length - 1; i++) {
     if (num[i] > num[i + 1]) {
@@ -23,6 +27,8 @@ const min = 138241
 const max = 674034
 let totalPasswords = 0
 
+// Generate a number, make sure it meets the requirements, then mark
+// if it's a valid password.
 for (let i = min; i <= max; i++) {
   // Convert to string...
   let num = i.toString()
